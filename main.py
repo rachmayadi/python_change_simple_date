0a226a5d5ffb8b1cd6f97ad2282eabc35562eeb6
teddy_r <teddy_r@binasanprima.com>
2021-06-08 15:17:56 +0700

    20210608 - initial

--- main.py
+++ main.py
@@ -11,16 +11,32 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def process_data(file_input=None):
+def load_data(file_input=None):
     dataframe_vals = pd.read_csv(file_input)
     if dataframe_vals:
         print('load done')
         return dataframe_vals
 
 
+def change_date_format(df_input=None):
+    changed_dataframe = False
+    return changed_dataframe
+
+
+def write_dataframe_to_csv(df_input=None,
+                           csv_filename=None):
+    return True
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    dataframe_vals = process_data(file_input="sales_records.csv")
-
+    dataframe_vals = load_data(file_input="sales_records.csv")
+    changed_dataframe = change_date_format(df_input=dataframe_vals)
+    write_result = write_dataframe_to_csv(df_input=changed_dataframe,
+                                          csv_filename="changed_sales_records.csv")
+    if write_result:
+        print('done')
+    else:
+        print('failed')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
